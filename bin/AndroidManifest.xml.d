9ed7be72839d5e72d423b9e17f3982746d493016
kevinboone <kevin@railwayterrace.com>
2019-06-12 11:53:15 +0100

    Started to update docs

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/kevin/source/androidmusicserver/bin/AndroidManifest.xml : \
-/home/kevin/source/androidmusicserver/AndroidManifest.xml \
--- bin/androidmusicserver-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/kevin/source/androidmusicserver/bin/androidmusicserver-debug-unaligned.apk : \
-/home/kevin/source/androidmusicserver/bin/androidmusicserver.ap_ \
-/home/kevin/source/androidmusicserver/bin/classes.dex \
